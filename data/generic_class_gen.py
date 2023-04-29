018c2460df54b77078858d4dc8dbc6d3e1163200
Laszlo Sari <laszlo.sari@edu.bme.hu>
2023-04-29 11:34:24 +0200

    dgl visualization

--- data/generic_class_gen.py
+++ data/generic_class_gen.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from typing import Generator
 
 import data
 from .common import *
@@ -11,16 +12,21 @@ import torch.nn.functional as F
 def node_match(u: dict, v: dict):
     return u['l'] == v['l']    
 
+
 def gen_dataset(num_g, min_num_nodes, max_num_nodes, edge_probability):
-    def gen_g():
-        yield from gen_smb_random(num_g, 5, min_num_nodes, max_num_nodes, edge_probability)
-    def gen_q():
-        yield from gen_smb_random(num_g, 1, 7, 8, 0.2, force_connected=True)
+    gen_g = gen_smb_random(num_g, 5, min_num_nodes, max_num_nodes, edge_probability)
+    gen_q = gen_smb_random(num_g, 1, 7, 8, 0.2, force_connected=True)
 
+    return gen_dataset(gen_g, gen_q)
 
+def gen_dataset(num_g: int, gen_g: Generator[nx.Graph, None, None]):
+    gen_q = gen_smb_random(num_g, 1, 7, 8, 0.2, force_connected=True)
+    return gen_dataset(gen_g, gen_q)
+
+def gen_dataset(gen_g: Generator[nx.Graph, None, None], gen_q: Generator[nx.Graph, None, None]):
     zipped_data = [] 
     
-    for g, q in zip(gen_g(), gen_q()):
+    for g, q in zip(gen_g, gen_q):
         # draw(g, "")
         # draw(q, "")
                 
@@ -41,6 +47,16 @@ def gen_dataset(num_g, min_num_nodes, max_num_nodes, edge_probability):
     return zipped_data
     
 
+def create_dataset_from_dgl(num_graphs):
+    def gen_g():
+        cora_dataset = CoraGraphDataset()
+        # return (dgl.to_networkx(g) for g in cora_dataset[:1])
+        g = cora_dataset[0]
+        g.ndata['l'] = torch.randint(0, 4, (g.num_nodes(),), dtype=torch.long)
+        yield dgl.to_networkx(g, node_attrs='l')
+    gen_q = gen_smb_random(num_graphs, 1, 7, 8, 0.2, force_connected=True)
+    return gen_dataset(gen_g(), gen_q)
+
 # create artifical data feature (= in degree) for each node
 def create_artificial_features(graph: nx.Graph):
     labels = [graph.nodes[v]['l'] for v in range(graph.number_of_nodes())]
@@ -53,6 +69,16 @@ def create_artificial_features(graph: nx.Graph):
     graph.ndata['feat'] = torch.cat([vertex_labels_one_hot, vertex_degrees], 1)    
     graph.edata['feat'] = torch.ones(graph.number_of_edges()).int()
     return graph
+
+def create_artificial_features_dgl(dgl_graph: DGLGraph):    
+    vertex_labels = torch.randint(0, 4, (dgl_graph.num_nodes(),), dtype=torch.long)
+    vertex_labels_one_hot = F.one_hot(vertex_labels, 4)    
+    vertex_degrees = dgl_graph.in_degrees().view(-1, 1).float()    
+
+    dgl_graph.ndata['label'] = vertex_labels
+    dgl_graph.ndata['feat'] = torch.cat([vertex_labels_one_hot, vertex_degrees], 1)    
+    dgl_graph.edata['feat'] = torch.ones(dgl_graph.number_of_edges()).int()
+    return dgl_graph
 
 def save_dataset(dataset, name):
     save_graphs(f'./data/{name}_generic_g.dat', [graph_query_label[0] for graph_query_label in dataset])
--- test.py
+++ test.py
@@ -6,6 +6,9 @@ import networkx.algorithms.isomorphism as nx_iso
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from dgl.data import CoraGraphDataset
+from gnnlens import Writer
+import torch
 
 from data import generic_class_gen
 from data.common import collate_graphs
@@ -16,6 +19,25 @@ model = torch.load('./models/model_generic_class_net_10000_medium_0.9009.pth')
 num_g = 64
 num_q = 8
 
+def draw(g):
+    g_nx = dgl.to_networkx(g)
+    pos = nx.drawing.layout.spring_layout(
+        g_nx,
+        k=2)
+    nx.draw_networkx(
+        g_nx,
+        pos=pos,
+        node_size=300,
+        edge_color='grey',
+        cmap=plt.cm.Reds,
+        vmin=0,
+        vmax=1,)
+
+    ax = plt.gca()
+    ax.margins(0.20)
+    plt.axis("off")
+    plt.show()
+
 def collate(samples):
     gs, qs, labels = map(list, zip(*samples))  # samples is a list of pairs (graph, label)
     labels = torch.cat(labels)
@@ -23,17 +45,23 @@ def collate(samples):
     batched_q = collate_graphs(qs)
     return batched_g, batched_q, labels
 
-dataset = generic_class_gen.load_dataset("generic_class_1000")
+# dataset = generic_class_gen.load_dataset("generic_class_1000")
+dataset = generic_class_gen.create_dataset_from_dgl(1)
+
+# writer = Writer('tutorial_graph')
+# nlabels = graph.ndata['label']
+# num_classes = 4
+# writer.add_graph(name='rnd', graph=graph,
+                # nlabels=nlabels, num_nlabel_types=num_classes)
+
+
 # dataset = generic_class_gen.gen_dataset(1, 20, 30, 0.1)
 # dataset = dataset[:10]
 # dataset_split = int(len(dataset) * 0.77)
 # testset = dataset[dataset_split:]
 test_loader = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=collate)
 threshold = 0.6
-
-loss = nn.BCELoss()
-
-import torch
+loss_fn = nn.BCELoss()
 
 
 def visualize(g, h):
@@ -42,14 +70,14 @@ def visualize(g, h):
         g_nx,
         k=2)
     nx.draw_networkx(
-        g_nx, 
-        pos=pos, 
+        g_nx,
+        pos=pos,
         node_size=300,
         edge_color='grey',
-        with_labels=True, 
-        node_color=h, 
-        cmap=plt.cm.Reds, 
-        vmin=0, 
+        with_labels=True,
+        node_color=h,
+        cmap=plt.cm.Reds,
+        vmin=0,
         vmax=1,)
 
     ax = plt.gca()
@@ -60,91 +88,73 @@ def visualize(g, h):
 def node_match(u: dict, v: dict):
     return (u['l'] == v['l']).item()
 
-def evaluate_accuracy(model, data_loader, loss, threshold, debug):
+def evaluate_accuracy(scores, labels, loss_fn, threshold):
+    J = loss_fn(scores, labels)
+    loss = J.detach().item()
+    classification = (scores > threshold).long()
+    acc = torch.sum(classification == labels).item()
+    nb_data = labels.size(0)
+
+    return (loss, acc, nb_data)
+
+def evaluate_iso(g, q, scores, threshold):
+    def get_label_from_feature(feature):        
+        return torch.argmax(feature[:, :-1], dim=1)
+
+    g.ndata['l'] = get_label_from_feature(g.ndata['feat'])
+    g_nx = g.to_networkx(node_attrs=['l'])
+
+    q.ndata['l'] = get_label_from_feature(q.ndata['feat'])
+    q_nx = q.to_networkx(node_attrs=['l'])
+
+    classification = scores > threshold
+    node_tensor = classification.nonzero()
+    nodes = node_tensor[:, :1].flatten().tolist()
+    g_reduced = g_nx.subgraph(nodes).copy()
+
+    match_ml = nx_iso.GraphMatcher(g_reduced, q_nx, node_match=node_match)
+    subisos_ml = match_ml.subgraph_isomorphisms_iter()
+    num_ml = len(list(subisos_ml)) + 1
+
+    match_orig = nx_iso.GraphMatcher(g_nx, q_nx, node_match=node_match)
+    subisos_orig = match_orig.subgraph_isomorphisms_iter()
+    num_orig = len(list(subisos_orig)) + 1
+
+    acc = num_ml / num_orig                        
+    return acc
+
+def save_visuals(batch_scores, writer, threshold):
+    classification = (batch_scores > threshold).long()
+    writer.add_model(graph_name='rnd', model_name='GNN',
+            nlabels=classification)
+
+
+def evaluate(model, data_loader, threshold):
     model.eval()
-    epoch_test_loss = 0
-    nb_data = 0
-    acc = 0        
-    
+
+    iso_acc = acc = nb_data = loss = 0
+
     with torch.no_grad():
         for iter, (batch_gs, batch_qs, batch_labels) in enumerate(data_loader):
             batch_X = batch_gs.graph.ndata['feat']
-            batch_E = batch_gs.graph.edata['feat']            
+            batch_E = batch_gs.graph.edata['feat']
 
             batch_X_q = batch_qs.graph.ndata['feat']
-            batch_E_q = batch_qs.graph.edata['feat']     
+            batch_E_q = batch_qs.graph.edata['feat']
 
             batch_scores = model(batch_gs, batch_X, batch_E, batch_qs, batch_X_q, batch_E_q)
-            J = loss(batch_scores, batch_labels)
 
-            if debug:
-                sc_orig = batch_scores # scaler.rescale(batch_scores)
-                labels_orig = batch_labels # scaler.rescale(batch_labels)
-                # visualize(batch_gs, sc_orig)
-                # print(sc_orig)
-                sc_predict = torch.clone(sc_orig)                
-                
-                for sc in sc_predict:
-                    sc[0] = 1.0 if sc[0] > threshold else 0.0                
-                # print(sc_predict)
-                # print(labels_orig)
+            # save_visuals(batch_scores)
+            curr_loss, curr_acc, curr_nb_data = evaluate_accuracy(batch_scores, batch_labels, loss_fn, threshold)
+            acc += curr_acc
+            loss += curr_loss
+            loss /= iter + 1
+            acc /= curr_nb_data
+            nb_data += curr_nb_data
+            iso_acc += evaluate_iso(batch_gs.graph, batch_qs.graph, batch_scores, threshold)
+            iso_acc /= iter + 1
+    return (iso_acc, acc, loss)
 
-            acc += torch.sum(sc_predict == labels_orig).item()
-            epoch_test_loss += J.detach().item()
-            nb_data += batch_labels.size(0)
 
-        epoch_test_loss /= (iter + 1)    
-        acc /= nb_data
-        print(acc)
-    
-    return epoch_test_loss
-
-def evaluate_iso(model, data_loader, loss, threshold, debug):
-    model.eval()
-    epoch_test_loss = 0
-    nb_data = 0
-    acc = 0        
-    
-    with torch.no_grad():
-        for iter, (batch_gs, batch_qs, batch_labels) in enumerate(data_loader):
-            batch_X = batch_gs.graph.ndata['feat']
-            batch_E = batch_gs.graph.edata['feat']            
-
-            batch_X_q = batch_qs.graph.ndata['feat']
-            batch_E_q = batch_qs.graph.edata['feat']     
-
-            batch_scores = model(batch_gs, batch_X, batch_E, batch_qs, batch_X_q, batch_E_q)
-            J = loss(batch_scores, batch_labels)
-
-            batch_gs.graph.ndata['l'] = torch.argmax(batch_X[:, :-1], dim=1)
-            g_nx = batch_gs.graph.to_networkx(node_attrs=['l'])            
-            
-            batch_qs.graph.ndata['l'] = torch.argmax(batch_X_q[:, :-1], dim=1)
-            q_nx = batch_qs.graph.to_networkx(node_attrs=['l'])            
-            
-            classification = batch_scores > threshold
-            node_tensor = classification.nonzero()
-            nodes = node_tensor[:, :1].flatten().tolist()
-            g_reduced = g_nx.subgraph(nodes).copy()
-            
-            match_ml = nx_iso.GraphMatcher(g_reduced, q_nx, node_match=node_match)
-            subisos_ml = match_ml.subgraph_isomorphisms_iter()       
-            num_ml = len(list(subisos_ml)) + 1
-
-            match_orig = nx_iso.GraphMatcher(g_nx, q_nx, node_match=node_match)
-            subisos_orig = match_orig.subgraph_isomorphisms_iter()       
-            num_orig = len(list(subisos_orig)) + 1
-
-            acc += num_ml / num_orig
-            epoch_test_loss += J.detach().item()
-            nb_data += 1
-
-        epoch_test_loss /= (iter + 1)    
-        acc /= nb_data
-        print(acc)
-    
-    return epoch_test_loss
-
-evaluate_iso(model, test_loader, loss, threshold, True)
-loss = evaluate_accuracy(model, test_loader, loss, threshold, True)
-print(loss)
+res = evaluate(model, test_loader, threshold)
+print(res)
